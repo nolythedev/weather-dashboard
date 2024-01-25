c05abc3b64702ee81aa9f73bdfe55ff6fed6c38e
nolythedev <nolychigwedere@hotmail.co.uk>
2024-01-25 22:38:00 +0000

    History buttons added

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
 
     function getDate() {
 
-        setInterval(function(){
+        setInterval(function () {
             var currentTime = dayjs()
             $('#date-time').text(currentTime.format('dddd D, MMMM HH:mm'))
         }, 1000)
-        
+
     }
 
     function displayWeather(event) {
@@ -15,23 +15,29 @@ $(document).ready(function () {
         event.preventDefault();
 
         // Grab the user input 
-        const userInput = $('#search-input').val();
+        const userInput = $('#search-input').val().trim();
 
         // Grab the API and assign user input 
         let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=301a6841ff749117c25d4794699b9037';
 
+        $('#search-input').val('');
         fetch(queryURL)
             .then(function (response) {
                 return response.json();
             }).then(function (data) {
                 console.log(data);
                 console.log(queryURL);
+
+                $('#locationName').text(data.name);
+
+                $('#history').append(`<button> ${data.name}, ${data.sys.country}</button>`);
+
             });
     }
 
 
 
-    
+
     $("#search-button").on('click', displayWeather);
     getDate();
 
